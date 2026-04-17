bbf6851a327cbeccb70e969f2a579bfcc1d03931
Andreita2007 <juanbri2905@gmail.com>
2026-04-17 11:30:11 -0400

    Agregando Manipulación de texto

--- TallerIujo01/TallerIujo01/Program.cs
+++ TallerIujo01/TallerIujo01/Program.cs
@@ -14,9 +14,25 @@ namespace TallerIujo01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("===Taller 01===");
 			
-			// TODO: Implement Functionality Here
+			// 1. El dato del usuario
+			
+			string registroUsuario = "     ID_777;     JuanPerez;     EVALUACIÓN;     95     ";
+
+			Console.WriteLine(registroUsuario);
+			
+			string registroLimpio = registroUsuario.Trim();
+			Console.WriteLine(registroLimpio);
+			
+			string[] partes = registroLimpio.Split(';');
+			string id = partes[0].Trim();
+			string nombre = partes[1].Trim();
+			string tarea = partes[2].Trim();
+			string nota = partes[3].Trim();
+			
+			Console.WriteLine(string.Format("El ID es : {0} del usuario: {1} con la nota: {2}", id, nombre,nota));
+			
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
